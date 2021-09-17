fc03c4df76a3edc6d9ecdc1b71298ec731635e06
AlejoErwin <alejo.gutierrez.erwin@gmail.com>
2021-09-16 21:02:19 -0400

    se modifico la base de datos

--- Covid_DB_create.sql
+++ Covid_DB_create.sql
@@ -3,123 +3,159 @@ create database covid_19_DB;
 USE covid_19_DB;
 
 CREATE TABLE City (
-    id_city int NOT NULL AUTO_INCREMENT,
-    city varchar(100) NOT NULL,
-    longitude numeric(15,10) NOT NULL,
-    latitude numeric(15,10) NOT NULL,
-    status int NOT NULL,
-    CONSTRAINT City_pk PRIMARY KEY (id_city)
+                      id_city int NOT NULL AUTO_INCREMENT,
+                      city varchar(100) NOT NULL,
+                      latitude numeric(15,10) NOT NULL,
+                      longitude numeric(15,10) NOT NULL,
+                      status int NOT NULL,
+                      tx_id int NOT NULL,
+                      tx_date datetime NOT NULL,
+                      tx_host varchar(100) NOT NULL,
+                      tx_update datetime NOT NULL,
+                      CONSTRAINT City_pk PRIMARY KEY (id_city)
 );
 
 -- Table: City_covid_data
 CREATE TABLE City_covid_data (
-    id_city_covid_data int NOT NULL,
-    id_city int NOT NULL,
-    id_covid_data int NOT NULL,
-    status int NOT NULL,
-    CONSTRAINT City_covid_data_pk PRIMARY KEY (id_city_covid_data)
+                                 id_city_covid_data int NOT NULL,
+                                 id_city int NOT NULL,
+                                 id_covid_data int NOT NULL,
+                                 status int NOT NULL,
+                                 tx_id int NOT NULL,
+                                 tx_date datetime NOT NULL,
+                                 tx_host varchar(100) NOT NULL,
+                                 tx_update datetime NOT NULL,
+                                 CONSTRAINT City_covid_data_pk PRIMARY KEY (id_city_covid_data)
 );
 
 -- Table: Country
 CREATE TABLE Country (
-    id_country int NOT NULL AUTO_INCREMENT,
-    name varchar(100) NOT NULL,
-    longitude float NOT NULL,
-    latitude float NOT NULL,
-    status int NOT NULL,
-    tx_id int NOT NULL,
-    tx_date datetime NOT NULL,
-    tx_host varchar(100) NOT NULL,
-    tx_update datetime NOT NULL,
-    CONSTRAINT Country_pk PRIMARY KEY (id_country)
+                         id_country int NOT NULL AUTO_INCREMENT,
+                         name varchar(100) NOT NULL,
+                         latitude float NOT NULL,
+                         longitude float NOT NULL,
+                         status int NOT NULL,
+                         tx_id int NOT NULL,
+                         tx_date datetime NOT NULL,
+                         tx_host varchar(100) NOT NULL,
+                         tx_update datetime NOT NULL,
+                         CONSTRAINT Country_pk PRIMARY KEY (id_country)
 );
 
 -- Table: Country_covid_data
 CREATE TABLE Country_covid_data (
-    id_city_covid_data int NOT NULL,
-    status int NOT NULL,
-    tx_id int NOT NULL,
-    tx_date datetime NOT NULL,
-    tx_host varchar(100) NOT NULL,
-    tx_update datetime NOT NULL,
-    id_country int NOT NULL,
-    id_covid_data int NOT NULL,
-    CONSTRAINT Country_covid_data_pk PRIMARY KEY (id_city_covid_data)
+                                    id_city_covid_data int NOT NULL,
+                                    status int NOT NULL,
+                                    id_country int NOT NULL,
+                                    id_covid_data int NOT NULL,
+                                    tx_id int NOT NULL,
+                                    tx_date datetime NOT NULL,
+                                    tx_host varchar(100) NOT NULL,
+                                    tx_update datetime NOT NULL,
+                                    CONSTRAINT Country_covid_data_pk PRIMARY KEY (id_city_covid_data)
 );
 
 -- Table: Covid_data
 CREATE TABLE Covid_data (
-    id_covid_data int NOT NULL AUTO_INCREMENT,
-    id_page_url int NOT NULL,
-    death_cases int NOT NULL,
-    confirmed_cases int NOT NULL,
-    vaccinated int NOT NULL,
-    cumulative_cases int NOT NULL,
-    recuperated int NOT NULL,
-    date datetime NOT NULL,
-    status int NOT NULL,
-    CONSTRAINT Covid_data_pk PRIMARY KEY (id_covid_data)
+                            id_covid_data int NOT NULL AUTO_INCREMENT,
+                            id_page_url int NOT NULL,
+                            death_cases int NOT NULL,
+                            confirmed_cases int NOT NULL,
+                            vaccinated int NOT NULL,
+                            cumulative_cases int NOT NULL,
+                            recuperated int NOT NULL,
+                            date datetime NOT NULL,
+                            status int NOT NULL,
+                            tx_id int NOT NULL,
+                            tx_date datetime NOT NULL,
+                            tx_host varchar(100) NOT NULL,
+                            tx_update datetime NOT NULL,
+                            CONSTRAINT Covid_data_pk PRIMARY KEY (id_covid_data)
 );
 
 -- Table: Drugstore
 CREATE TABLE Drugstore (
-    id_drugstore int NOT NULL,
-    id_city int NOT NULL,
-    name varchar(50) NOT NULL,
-    lon float NOT NULL,
-    lat float NOT NULL,
-    status int NOT NULL,
-    CONSTRAINT Drugstore_pk PRIMARY KEY (id_drugstore)
+                           id_drugstore int NOT NULL,
+                           id_city int NOT NULL,
+                           name varchar(50) NOT NULL,
+                           lon float NOT NULL,
+                           lat float NOT NULL,
+                           status int NOT NULL,
+                           tx_id int NOT NULL,
+                           tx_date datetime NOT NULL,
+                           tx_host varchar(100) NOT NULL,
+                           tx_update datetime NOT NULL,
+                           CONSTRAINT Drugstore_pk PRIMARY KEY (id_drugstore)
 );
 
 -- Table: Hospital
 CREATE TABLE Hospital (
-    id_hospital int NOT NULL,
-    id_city int NOT NULL,
-    name varchar(50) NOT NULL,
-    lon float NOT NULL,
-    lat float NOT NULL,
-    status int NOT NULL,
-    CONSTRAINT Hospital_pk PRIMARY KEY (id_hospital)
+                          id_hospital int NOT NULL,
+                          id_city int NOT NULL,
+                          name varchar(50) NOT NULL,
+                          lon float NOT NULL,
+                          lat float NOT NULL,
+                          status int NOT NULL,
+                          tx_id int NOT NULL,
+                          tx_date datetime NOT NULL,
+                          tx_host varchar(100) NOT NULL,
+                          tx_update datetime NOT NULL,
+                          CONSTRAINT Hospital_pk PRIMARY KEY (id_hospital)
 );
 
 -- Table: Municipality
 CREATE TABLE Municipality (
-    id_municipality int NOT NULL AUTO_INCREMENT,
-    id_city int NOT NULL,
-    municipality varchar(100) NOT NULL,
-    longitude float NOT NULL,
-    latitude float NOT NULL,
-    status int NOT NULL,
-    CONSTRAINT Municipality_pk PRIMARY KEY (id_municipality)
+                              id_municipality int NOT NULL AUTO_INCREMENT,
+                              id_city int NOT NULL,
+                              municipality varchar(100) NOT NULL,
+                              latitude float NOT NULL,
+                              longitude float NOT NULL,
+                              status int NOT NULL,
+                              tx_id int NOT NULL,
+                              tx_date datetime NOT NULL,
+                              tx_host varchar(100) NOT NULL,
+                              tx_update datetime NOT NULL,
+                              CONSTRAINT Municipality_pk PRIMARY KEY (id_municipality)
 );
 
 -- Table: Municipality_covid_data
 CREATE TABLE Municipality_covid_data (
-    id_municipality_covid_data int NOT NULL,
-    id_municipality int NOT NULL,
-    id_covid_data int NOT NULL,
-    status int NOT NULL,
-    CONSTRAINT Municipality_covid_data_pk PRIMARY KEY (id_municipality_covid_data)
+                                         id_municipality_covid_data int NOT NULL,
+                                         id_municipality int NOT NULL,
+                                         id_covid_data int NOT NULL,
+                                         status int NOT NULL,
+                                         tx_id int NOT NULL,
+                                         tx_date datetime NOT NULL,
+                                         tx_host varchar(100) NOT NULL,
+                                         tx_update datetime NOT NULL,
+                                         CONSTRAINT Municipality_covid_data_pk PRIMARY KEY (id_municipality_covid_data)
 );
 
 -- Table: Page_url
 CREATE TABLE Page_url (
-    id_page_url int NOT NULL AUTO_INCREMENT,
-    url varchar(300) NOT NULL,
-    status int NOT NULL,
-    CONSTRAINT Page_url_pk PRIMARY KEY (id_page_url)
+                          id_page_url int NOT NULL AUTO_INCREMENT,
+                          url varchar(300) NOT NULL,
+                          status int NOT NULL,
+                          tx_id int NOT NULL,
+                          tx_date datetime NOT NULL,
+                          tx_host varchar(100) NOT NULL,
+                          tx_update datetime NOT NULL,
+                          CONSTRAINT Page_url_pk PRIMARY KEY (id_page_url)
 );
 
 -- Table: User
 CREATE TABLE User (
-    id_user int NOT NULL AUTO_INCREMENT,
-    user_name varchar(100) NOT NULL,
-    last_name varchar(100) NOT NULL,
-    email varchar(100) NOT NULL,
-    password varchar(60) NOT NULL,
-    status int NOT NULL,
-    CONSTRAINT id_user PRIMARY KEY (id_user)
+                      id_user int NOT NULL AUTO_INCREMENT,
+                      user_name varchar(100) NOT NULL,
+                      last_name varchar(100) NOT NULL,
+                      email varchar(100) NOT NULL,
+                      password varchar(60) NOT NULL,
+                      status int NOT NULL,
+                      tx_id int NOT NULL,
+                      tx_date datetime NOT NULL,
+                      tx_host varchar(100) NOT NULL,
+                      tx_update datetime NOT NULL,
+                      CONSTRAINT id_user PRIMARY KEY (id_user)
 );
 
 -- foreign keys
---
+++ covid_DB_insert.sql
@@ -0,0 +1,14 @@
+
+
+START TRANSACTION;
+insert into city values (null, 'La Paz', -15.1277648, -67.3182591, 1, 1,now(), '198.168.0.1', now());
+insert into city values (null, 'Santa Cruz', -17.0148376, -61.5580529, 1,1,now(), '198.168.0.1', now());
+insert into city values (null, 'Oruro', -18.1998846, -67.8253777, 1,1,now(), '198.168.0.1', now());
+insert into city values (null, 'Potosí', -20.0880834, -66.300821, 1, 1,now(), '198.168.0.1', now());
+insert into city values (null, 'Chuquisaca', -19.7915578, -64.030858, 1, 1,now(), '198.168.0.1', now());
+insert into city values (null, 'Cochabamba', -17.0653827, -65.5062222, 1, 1,now(), '198.168.0.1', now());
+insert into city values (null, 'Tarija', -21.5915756, -63.91808, 1,1,now(), '198.168.0.1', now());
+insert into city values (null, 'Pando', -11.1673451, -67.2601429, 1, 1,now(), '198.168.0.1', now());
+insert into city values (null, 'Beni', -13.4951348, -64.8929298, 1, 1,now(), '198.168.0.1', now());
+COMMIT;
+
