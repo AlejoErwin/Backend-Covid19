b6513ccac9b23dbda1043c562adfbe6eb59d83d6
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2021-09-20 21:01:48 -0400

    Modificando la clase de la conexion de los datos

--- src/main/java/software/Backend/Covid19/bl/util/CovidDataUtil.java
+++ src/main/java/software/Backend/Covid19/bl/util/CovidDataUtil.java
@@ -4,6 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import software.Backend.Covid19.dao.CountryDao;
 
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.ArrayList;
 
 @Component
@@ -16,6 +20,17 @@ public class CovidDataUtil {
     }
 
     public ArrayList<ArrayList> getVaccine(String country, Integer length){
+        ArrayList<ArrayList> vaccineData = new ArrayList();
+        try {
+            URL url = new URL("https://disease.sh/v3/covid-19/vaccine/coverage/countries/"+country+"?lastdays="+length.toString());
+
+            Calendar c = Calendar.getInstance();
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
         return null;
     }
 
@@ -23,4 +38,11 @@ public class CovidDataUtil {
         return null;
     }
 
+    /*
+    public static String formatCalender(Calender c){
+        DataFormat dataFormat = new SimpleDateFormat("M/d/yy");
+        return dataFormat.format(c.getTime());
+    }
+
+     */
 }
