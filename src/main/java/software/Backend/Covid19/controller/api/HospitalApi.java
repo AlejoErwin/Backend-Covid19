2484455800873c41f3824e8cf1727d43e05421f2
jorgeluisAG <jorge.luis.alejo.gutierrez@gmail.com>
2021-09-25 23:19:42 -0400

    Modificando las clases y agregando dependencias al pom

--- src/main/java/software/Backend/Covid19/controller/api/HospitalApi.java
+++ src/main/java/software/Backend/Covid19/controller/api/HospitalApi.java
@@ -1,5 +1,6 @@
 package software.Backend.Covid19.controller.api;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/software/Backend/Covid19/dao/HospitalDao.java
+++ src/main/java/software/Backend/Covid19/dao/HospitalDao.java
@@ -1,5 +1,6 @@
 package software.Backend.Covid19.dao;
 
+
 import org.apache.ibatis.annotations.Mapper;
 import software.Backend.Covid19.shared.dto.HospitalRequest;
 import software.Backend.Covid19.shared.model.Hospital;
